ef2e45d8a8dd8b0944d46435d15389dc0021b627
JohnFolsonner <73367806+JohnFolsonner@users.noreply.github.com>
2021-02-13 10:12:57 +0300

    Create JF_14task.py

---
+++ JF_14task.py
@@ -0,0 +1,43 @@
+#########################################################
+#                -==John Folsonner==-                   #
+#             -==14 task of the exam==-                 #
+#########################################################
+
+# /IMPORT BLOCK/
+import math
+# /IMPORT BLOCK/
+
+# /DEF BLOCK/
+def convert(num, to_base=2):
+    conv_num = 0
+    X = 1
+    while num:
+        conv_num += (num % to_base) * X
+        X *= 10
+        num //= to_base
+    return conv_num
+
+def sum(num):
+    s = 0
+    while num:
+        s += num % 10
+        num //= 10
+    return s
+
+def IsPrime(num):
+    for i in range(2, int(math.sqrt(num))+1):
+        if num % i == 0:
+            return False
+    return True
+# /DEF BLOCK/
+
+# /Variable block/
+SUMM = 0
+# /Variable block/
+
+for i in range(2, 11):
+    num = convert(437, to_base=i)
+    if IsPrime(sum(num)) == True:
+        SUMM += i
+
+print(SUMM)
